c1e75a76ccf992c286128c27563a4525c17c0b79
Justin Henry <justin.henry@uvm.edu>
2020-10-22 11:28:07 -0400

    Refactoring to use new encoder hook.

--- EncodeTo.php
+++ EncodeTo.php
@@ -43,34 +43,30 @@ class EncodeTo extends PluginAbstract
   public function load()
   {
     // Starting at top of upload completion controller, b/c we still have a videoId in the session vars
-    Plugin::attachEvent('upload_complete.start', array(__CLASS__, 'hd_encode'));
-    Plugin::attachEvent('upload_complete.start', array(__CLASS__, 'createSMIL'));
+    Plugin::attachEvent('encoder.after.thumbnail', array(__CLASS__, 'hd_encode'));
+    Plugin::attachEvent('encoder.after.thumbnail', array(__CLASS__, 'createSMIL'));
   }
 
   /**
    * Encode an HD720p version of the video.
    * TODO: validate user, etc
    */
-  public static function hd_encode()
+  public static function hd_encode($video)
   {
-	  if (class_exists('Wowza')) {
-		  Filesystem::createDir(UPLOAD_PATH . '/HD720/');
-	  }
+    $type = "H.264 720p";
 
-	  $video = EncodeTo::getVideoToEncode();
+    if (class_exists('Wowza')) {
+      Filesystem::createDir(UPLOAD_PATH . '/HD720/');
+    }
 
-	  if (!EncodeTo::isAudio($video)) {
-		  $config = Registry::get('config');
-		  $commandOutput = $config->debugConversion ? CONVERSION_LOG : '/dev/null';
-		  $command = Settings::get('php') . ' ' . DOC_ROOT . '/cc-content/plugins/EncodeTo/encode.php --video="' . $video->videoId . '" >> ' .  $commandOutput . ' 2>&1 &';
+    $config = Registry::get('config');
+    $config->debugConversion ? App::log(CONVERSION_LOG, "\n$type Converter Called For Video: $video->videoId") : null;
 
-		  if (class_exists('QEncoder')) 
-		  {
-			  $command = QEncoder::q_encoder($command);
-		  }
-
-		  exec('nohup ' . $command);
-	  }
+    if (!EncodeTo::isAudio($video)) {
+      $HDPaths = EncodeTo::getHDPaths($video);
+      EncodeTo::encode($video, $type);
+      EncodeTo::cleanup($HDPaths, $video, $type);
+    }
   }
 
   /**
@@ -89,21 +85,244 @@ class EncodeTo extends PluginAbstract
     }
   }
 
-/**
- * Checks to see if this is an audio file and that audio is allowed.
- * @param Video $video The video object to inspect.
- * @return bool True if audio's allowed and it's an audio file.
- */
-private static function isAudio(Video $video) 
-{
+  /**
+   * Checks to see if this is an audio file and that audio is allowed.
+   * @param Video $video The video object to inspect.
+   * @return bool True if audio's allowed and it's an audio file.
+   */
+  private static function isAudio(Video $video) 
+  {
     if( class_exists( 'EnableAudio' ) ) {
-        $audioFormats = json_decode(Settings::get('enable_audio_formats'));
-        if( in_array($video->originalExtension, $audioFormats) ) {
-            return true;
-        }
+      $audioFormats = json_decode(Settings::get('enable_audio_formats'));
+      if( in_array($video->originalExtension, $audioFormats) ) {
+        return true;
+      }
     }
     return false;
-}
+  }
+
+
+  /**
+   * Encode to HD720p 
+   * 
+   * @param Video $video Video object
+   * @param string $type type of file, i.e. temp, final, HD, mp3, etc. 
+   */
+  public static function encode($video, $type)
+  {
+    $config = Registry::get('config');
+
+    $encoderPaths = EncodeTo::getEncoderPaths($video);
+    $HDPaths = EncodeTo::getHDPaths($video, $encoderPaths);
+    extract($HDPaths);
+
+    // Build the encoder command.
+    $config->debugConversion ? App::log(CONVERSION_LOG, "\nPreparing for $type Encoding...") : null;
+    $command = "$ffmpegPath -i $rawVideo " . Settings::get('HD720_encoding_options') . " $tempFilePath >> $debugLogPath 2>&1";
+
+    EncodeTo::debugLog("$type Encoding", $debugLogPath, $command);
+
+    // Execute encoding command
+    exec($command);
+
+    EncodeTo::validateFileCreation($tempFilePath, $video, "temp $type");
+    EncodeTo::shiftMoovAtom($encoderPaths, $video, $type);
+  }
+
+  /**
+   * Set HD720 encoder paths from configs and filenames.
+   * 
+   * @param array $encoderPaths keyed array of path variables
+   * @param Video $video Video object
+   */
+  public static function getHDPaths($video, $encoderPaths = [], $HdDir = "/HD720/")
+  {
+    $baseHDPath = UPLOAD_PATH . $HdDir;
+    $encoderPaths["rawVideo"] = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->originalExtension;
+    $encoderPaths["tempFilePath"] = $baseHDPath . $video->filename . '_temp.mp4';
+    $encoderPaths["filePath"] = $baseHDPath . $video->filename . '.mp4';
+    $encoderPaths["smilPath"] = UPLOAD_PATH . '/' . $video->filename . '.smil';
+
+    return $encoderPaths;
+  }
+
+  /**
+   * Set HD720 encoder paths from configs and filenames.
+   * 
+   * @param array $encoderPaths keyed array of path variables
+   * @param Video $video Video object
+   */
+  public static function validateVideo($video, $type)
+  {
+    $config = Registry::get('config');
+
+    // Validate requested video
+    $config->debugConversion ? App::log(CONVERSION_LOG, "Validating requested $type video...") : null;
+    if (!$video) throw new Exception("An invalid video was passed to the $type video encoder.");
+
+    // Retrieve video path information
+    $config->debugConversion ? App::log(CONVERSION_LOG, 'Establishing variables...') : null;
+    $HDPaths = EncodeTo::getHDPaths($video);
+    extract($HDPaths);
+
+    // Verify Raw Video Exists
+    $config->debugConversion ? App::log(CONVERSION_LOG, "Verifying raw $type video exists...") : null;
+    if (!file_exists($rawVideo)) throw new Exception("The raw $type video file does not exists. The id of the video is: $video->videoId");
+
+    // Verify Raw Video has valid file size
+    // (Greater than min. 5KB, anything smaller is probably corrupted
+    $config->debugConversion ? App::log(CONVERSION_LOG, 'Verifying raw video was valid size...') : null;
+    if (!filesize($rawVideo) > 1024 * 5) throw new Exception("The raw $type video file is not a valid filesize. The id of the video is: $video->videoId");
+
+    return $video;
+  }
+
+  /**
+   * Shift Moov atom for faster streaming start times.
+   * 
+   * @param array $configs array of config variables. 
+   * @param Video $video Video object
+   * @param string $type type of file, i.e. temp, final, HD, mp3, etc. 
+   */
+  public static function shiftMoovAtom($encoderVars, $video, $type)
+  {
+
+    $encoderPaths = EncodeTo::getHDPaths($video, $encoderVars);
+    extract($encoderPaths);
+
+    // Debug Log
+    $config = Registry::get('config');
+    $config->debugConversion ? App::log(CONVERSION_LOG, "\nShifting moov atom on $type video...") : null;
+
+    // Prepare shift moov atom command
+    $shiftMoovAtomCommand = "$qt_faststart_path $tempFilePath $filePath >> $debugLogPath 2>&1";
+
+    EncodeTo::debugLog("$type Shift Moov Atom", $debugLogPath, $shiftMoovAtomCommand);
+
+    // Execute shift moov atom command
+    exec($shiftMoovAtomCommand);
+
+    EncodeTo::validateFileCreation($filePath, $video, "final $type");
+  }
+
+  /**
+   * Check to see if the file was created and output logs if not.
+   * 
+   * @param string $path path to file
+   * @param Video $video Video object
+   * @param string $type type of file, i.e. temp, final, HD, mp3, etc. 
+   * TODO: log out exception
+   */
+  public static function validateFileCreation($path, $video, $type)
+  {
+    $config = Registry::get('config');
+    $config->debugConversion ? App::log(CONVERSION_LOG, "Verifying $type file was created...") : null;
+
+    // Verify file was created successfully
+    if (!file_exists($path) || filesize($path) < 1024 * 5) {
+      throw new Exception("The $type file  for user $video->userId $user->username was not created. The id of the video is: $video->videoId $video->title");
+    }
+  }
+
+  /**
+   * Set encoder paths from configs and filenames.
+   * 
+   * @param Video $video Video object
+   */
+  public static function getEncoderPaths($video)
+  {
+    $ffmpegPath = Settings::get('ffmpeg');
+    $qt_faststart_path = Settings::get('qtfaststart');
+    $debugLogPath = LOG . '/' . $video->filename . '.log';
+    $rawVideo = UPLOAD_PATH . '/temp/' . $video->filename . '.' . $video->originalExtension;
+
+    return compact('ffmpegPath', 'qt_faststart_path', 'debugLogPath', 'rawVideo');
+  }
+
+  /**
+   * Check permissions on transcoding binaries
+   * 
+   * @param string $label label/type of binary (i.e. FFMPEG vs qt-faststart).
+   * @param string $path path to binary 
+   * 
+   */
+  public static function checkTranscoderPermissions($label, $path)
+  {
+    $config = Registry::get('config');
+    $config->debugConversion ? App::log(CONVERSION_LOG, "\nChecking $label permissions...") : null;
+    if (strpos($path, DOC_ROOT) !== false && Filesystem::getPermissions($path) != '0777') {
+      try {
+        Filesystem::setPermissions($path, 0777);
+      } catch (Exception $e) {
+        throw new Exception("Unable to update permissions for $label. Please make sure $path has 777 executeable permissions.\n\nAdditional information: " . $e->getMessage());
+      }
+    }
+  }
+
+  /**
+   * Clean up temp files and/or logs
+   * 
+   */
+  public static function cleanup($encoderPaths, $video, $type)
+  {
+    $config = Registry::get('config');
+    $HDPaths = EncodeTo::getHDPaths($video, $encoderPaths);
+    extract($HDPaths);
+    try {
+      // Delete pre-faststart files
+      $config->debugConversion ? App::log(CONVERSION_LOG, 'Deleting temp file for $type video...') : null;
+      Filesystem::delete($tempFilePath);
+
+    } catch (Exception $e) {
+      App::alert("Error During $type Video Encoding Cleanup for video: $video->videoId", $e->getMessage());
+      App::log(CONVERSION_LOG, $e->getMessage());
+    }
+  }
+
+  /**
+   * Handle general encoder failure/exception 
+   * 
+   */
+  public static function handleException($exception, $video = false, $importJobId = 0)
+  {
+    // Update video status
+    if ($video) {
+      $video->status = \VideoMapper::FAILED;
+      $video->jobId = null;
+      $videoMapper->save($video);
+    }
+
+    // Notify import script of error
+    if ($importJobId) {
+      \ImportManager::executeImport($importJobId);
+    }
+
+    App::alert('Error During Video Encoding', $exception->getMessage());
+    App::log(CONVERSION_LOG, $exception->getMessage());
+    exit();
+  }
+
+
+  /**
+   * Format and output log info.
+   * 
+   * @param string $label label/type of log.
+   * @param string $message log content
+   * 
+   */
+  public static function debugLog($logType, $logPath, $command)
+  {
+    $config = Registry::get('config');
+    $logMessage = "\n\n\n\n==================================================================\n";
+    $logMessage .= "$logType\n";
+    $logMessage .= "==================================================================\n\n";
+    $logMessage .= "$logType Command: $command\n\n";
+    $logMessage .= "$logType Output:\n\n";
+    $config->debugConversion ? App::log(CONVERSION_LOG, "$logType Command: " . $command) : null;
+    App::log($logPath, $logMessage);
+  }
+
+
 
   /**
    * Create SMIL file.
@@ -111,24 +330,12 @@ private static function isAudio(Video $video)
    * @param array $smilPath path to smil file
    * @param Video $video Video object
    */
-  public static function createSMIL()
+  public static function createSMIL($video)
   {
-    $video = EncodeTo::getVideoToEncode();
     $smilPath = UPLOAD_PATH . '/' . $video->filename . '.smil';
     $config = Registry::get('config');
     $config->debugConversion ? App::log(CONVERSION_LOG, "\nCreating SMIL file at $smilPath") : null;
-    $content = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>
-	<smil title=''>
-	<head>
-	</head>
-		<body>
-			<switch>
-        <video height='300' src='mobile/$video->filename.mp4' systemLanguage='eng' width='480'  title='Low'  system-bitrate='644100' />
-        <video height='360' src='h264/$video->filename.mp4' systemLanguage='eng' width='640'   title='Medium' system-bitrate='1068100' />
-        <video height='720' src='HD720/$video->filename.mp4' systemLanguage='eng' width='1280' title='High' system-bitrate='1544100' />
-			</switch>
-		</body>
-  </smil>";
+    $content = include 'smil.php'; 
     try{
       Filesystem::create($smilPath);
       Filesystem::write($smilPath,$content, false);
@@ -136,5 +343,5 @@ private static function isAudio(Video $video)
       $config->debugConversion ? App::log(CONVERSION_LOG, "\nERROR - Problem creating SMIL file at $smilPath: $e") : null;
     }
   }
-  
+
 }
---
+++ smil.php
@@ -0,0 +1,14 @@
+<?php
+$smil = "<smil>
+	<head>
+	</head>
+		<body>
+			<switch>
+        <video height='300' src='mobile/$video->filename.mp4' systemLanguage='eng' width='480'  title='Low'  system-bitrate='644100' />
+        <video height='360' src='h264/$video->filename.mp4' systemLanguage='eng' width='640'   title='Medium' system-bitrate='1068100' />
+        <video height='720' src='HD720/$video->filename.mp4' systemLanguage='eng' width='1280' title='High' system-bitrate='1544100' />
+			</switch>
+		</body>
+</smil>";
+
+return $smil;
