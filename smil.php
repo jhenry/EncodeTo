35d9b898e0e43bce6dbf9ccbd3c0cf1308093e30
Justin Henry <justin.henry@uvm.edu>
2020-10-26 17:31:26 -0400

    Don't write video tag for audio files..

--- smil.php
+++ smil.php
@@ -5,9 +5,11 @@ $smil = "<smil>
 		<body>
 			<switch>
         <video height='300' src='mobile/$video->filename.mp4' systemLanguage='eng' width='480'  title='Low'  system-bitrate='644100' />
-        <video height='360' src='h264/$video->filename.mp4' systemLanguage='eng' width='640'   title='Medium' system-bitrate='1068100' />
-        <video height='720' src='HD720/$video->filename.mp4' systemLanguage='eng' width='1280' title='High' system-bitrate='1544100' />
-			</switch>
+        <video height='360' src='h264/$video->filename.mp4' systemLanguage='eng' width='640'   title='Medium' system-bitrate='1068100' />";
+if (!EncodeTo::isAudio($video)) {
+        $smil .= "<video height='720' src='HD720/$video->filename.mp4' systemLanguage='eng' width='1280' title='High' system-bitrate='1544100' />";
+}
+			$smil .="</switch>
 		</body>
 </smil>";
 
